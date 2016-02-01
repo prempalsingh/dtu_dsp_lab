6668c5384026f4bdc3802737cb2a1a4c83e7017a
Prempal Singh <prempal.42@gmail.com>
2016-02-01 12:05:00 +0530

    Create expt3.m

---
+++ expt3.m
@@ -0,0 +1,16 @@
+clc;
+clear all;
+close all;
+x=input('enter the 1st sequence: ');
+h=input('enter the 2nd sequence: ');
+y=conv(x,h);
+figure;subplot(3,1,1);
+stem(x);ylabel('Amplitude');
+xlabel('(a) n');
+subplot(3,1,2);
+stem(h);ylabel('Amplitude');
+xlabel('(b) n');
+subplot(3,1,3);
+stem(y);ylabel('Amplitude');
+xlabel('(c) n');
+disp('The resultant signal is');
